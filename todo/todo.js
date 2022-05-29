b1c2e72541f4b5894edb74ea79e7b57ed4dbccd4
moscarelloscott <38712754+moscarelloscott@users.noreply.github.com>
2022-05-29 03:32:22 -0400

    Add files via upload

--- todo/todo.js
+++ todo/todo.js
@@ -4,12 +4,14 @@ const todobutton = document.querySelector(".todoButton");
 const todolist = document.querySelector(".todoList");
 
 //event listener
+//when the todoButton is pressed the addTodo function will run
 todobutton.addEventListener("click", addTodo);
+//misleading name, will run the deletion and strike through functions
 todolist.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo(event){
-    console.log("test");
+    //console.log("test");
     // prevents form from submitting so text stays in console
     event.preventDefault();
     //todo div
@@ -37,7 +39,7 @@ function addTodo(event){
      //clearing values
      todoinput.value = "";
 }
-
+// deleteCheck will remove the class todo it is releated to
 function deleteCheck(e) {
     // show item being clicked
     //console.log(e.target)
@@ -47,11 +49,10 @@ function deleteCheck(e) {
         todo.remove();
     }
 
-    // cross out
+    // this function will create a completed class for the todo text...strike thru in CSS
     if(item.classList[0] === "complete-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
-
     }
 }
 
